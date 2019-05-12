f17094dc63f6dfb77a301958c95374caf8338c42
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-05-12 13:07:23 +0300

    create poll form

--- client/src/components/CreatePoll.js
+++ client/src/components/CreatePoll.js
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 
 
 class CreatePoll extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            question: '',
+            options: ['', '']
+        }
+    }
+
+    handleAddOption = (e) => { this.setState({ options: [...this.state.options, '']})}
+    handleSubmit = (e) => { 
+        e.preventDefault()
+        const { question, options } = this.state
+        console.log({ question, options })
+    }
+    handleChange = (e) => { this.setState({ [e.target.name]: e.target.value })}
+    handleChangeOption = (e, index) => {
+        const options = [...this.state.options]
+        options[index] = e.target.value
+        this.setState({ options })
+    }
+
     render () {
+        const options = this.state.options.map((option, i) => (
+            <Fragment key={i}>
+                <label className="form-label">option {i+1}</label>
+                <input 
+                    className="form-input"
+                    type="text"
+                    value={option}
+                    onChange={(e) => this.handleChangeOption(e, i)}
+                />
+            </Fragment>
+        ))
         return (
-            <div>
-                This is CreatePoll Form 
-            </div>
+            <form className="form" onSubmit={this.handleSubmit}>
+                <label className="form-label" htmlFor="question">question</label>
+                <input
+                    className="form-input"
+                    name="question"
+                    type="text"
+                    value={this.state.question}
+                    onChange={this.handleChange}
+                />
+                <div className="container">{options}</div>
+
+                <div className="button-center">
+                    <button 
+                        type="button" 
+                        className="button"
+                        onClick={this.handleAddOption}
+                    >
+                        Add option
+                    </button>
+                    <button type="submit" className="button">Submit</button>
+                </div>
+            </form>
         )
     }
 }
