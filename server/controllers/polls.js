c9326812a96b9d19148aa147f32dcb315d9d623f
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-05-16 20:31:41 +0300

    get poll and get all polls server

--- server/controllers/polls.js
+++ server/controllers/polls.js
@@ -1,7 +1,26 @@
 const User = require('../models/User')
 const Poll = require('../models/Poll')
 
+exports.getAll = async function (req, res, next) {
+    try {
+        const polls = await Poll.find({}).populate('user', ['username', 'id'])
+        return res.status(200).json(polls)
+    } catch (err) {
+        return res.status(400).json({ message: err.message })
+    }
+}
 
+exports.getPoll = async function (req, res, next) {
+    try {
+        const poll = await Poll.findById(req.params.pollId).populate('user', ['username', 'id'])
+        if (!poll) {
+            throw new Error('No poll found.')
+        }
+        return res.status(200).json(poll)
+    } catch (err) {
+        return res.status(400).json({ message: err.message })
+    }
+}
 exports.create = async function (req, res, next) {
     const { id } = req.decoded
     const { question, options } = req.body
--- server/routes/polls.js
+++ server/routes/polls.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const jwtAuth = require('../middleware/auth')
 const pollController = require('../controllers/polls')
 
+router.get('/', pollController.getAll)
 router.post('/new', jwtAuth, pollController.create)
+router.get('/:pollId', pollController.getPoll)
 
 module.exports = router 
